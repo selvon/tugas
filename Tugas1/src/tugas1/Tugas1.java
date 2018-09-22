0b2a48d3d36e64cbb274c1564537376eccf9f009
Selvon <43486621+selvon@users.noreply.github.com>
2018-09-22 08:57:42 +0700

    Add files via upload

---
+++ Tugas1/src/tugas1/Tugas1.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package tugas1;
+
+/**
+ *
+ * @author christselvon
+ */
+public class Tugas1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        int umur = 12;
+        boolean cantik = false;
+        
+        if (umur > 23 && cantik ){
+            System.out.println("Mau banget!");
+        }else if (umur > 23 && cantik==false){
+            System.out.println("pikir dulu");
+        }else{
+            System.out.println("No way!");
+        } 
+    }
+    
+}
